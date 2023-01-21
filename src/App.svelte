c25747fb24d7defa1352081d59272dfceeace397
yugure <98769788+everlastingsong@users.noreply.github.com>
2023-01-22 02:37:37 +0900

    add repository url

--- src/App.svelte
+++ src/App.svelte
@@ -3,28 +3,33 @@
   import { routes } from "./router";
   import RpcSelector from "./components/RpcSelector.svelte";
   import { rpcConnection } from "./stores";
+
+  const repositoryURL = "https://github.com/everlastingsong/ts-svelte-webpack-orca-starter";
 </script>
 
 <main>
   <div style="text-align: right;"><RpcSelector /></div>
-  <h1 style="margin: 0em;"><a href="#/">🏠</a> ts-svelte-webpack-orca-starter </h1>
+  <h1 style="margin: 0em;">
+    <a href="#/">🏠</a> ts-svelte-webpack-orca-starter
+  </h1>
+  <span style="font-size: x-small">Repository : <a href={repositoryURL}>{repositoryURL}</a></span>
   <hr />
   {#key $rpcConnection} <!-- re-render if rpcConnection is changed -->
-  <Router {routes} />
+    <Router {routes} />
   {/key}
 </main>
 
 <style>
-	main {
-		text-align: left;
-		padding: 0.2em 0.2em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: left;
+    padding: 0.2em 0.2em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
   @media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    main {
+      max-width: none;
+    }
+  }
 </style>
